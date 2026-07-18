0f6843a8b3758e491d91c1fbcbed1cf31fb77ee1
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-07-18 23:17:36 +0530

    Narrowing casting

---
+++ Day5.java
@@ -0,0 +1,9 @@
+public class Day5 {
+  public static void main(String[] args) {
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; // Explicit casting: double to int
+
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
