d29d701227bc9bdb3696d85e0fec34d51caa4384
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-07-18 23:49:44 +0530

    Narrowing casting

---
+++ Day7.java
@@ -0,0 +1,11 @@
+
+
+public class Day7 {
+    public static void main(String[] args){
+        double myDouble=9.876;
+        int myInt=(int) myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+    
+}
