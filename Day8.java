251a3f233405d22e1e53f9b6f85368b88c9cc59e
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-07-19 10:48:04 +0530

    People ++/--

---
+++ Day8.java
@@ -0,0 +1,18 @@
+
+
+public class Day8 {
+    public static void main(String[] args){
+        int peopleInroom=0;
+
+        peopleInroom++;
+        peopleInroom++;
+        peopleInroom++;
+
+        System.out.println(peopleInroom);
+
+        peopleInroom--;
+        System.out.println(peopleInroom);
+    }
+}
+    
+
