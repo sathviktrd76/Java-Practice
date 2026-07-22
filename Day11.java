f3a3accc7e495be250df7c60d1c7bbcc653e14e5
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-07-23 00:16:18 +0530

    Swapping

---
+++ Day11.java
@@ -0,0 +1,14 @@
+public class Day11 {
+    public static void main(String[] args){
+        int a=7;
+        int b=6;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping : ");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+    
+}
