76292c72c315c5488418b56720ff8bc8465fd20c
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-07-22 23:54:51 +0530

    Java widening casting

---
+++ Day10.java
@@ -0,0 +1,12 @@
+
+
+public class Day10 {
+    //widening casting
+    public static void main(String[] args){
+        int myInt=10;
+        double myDouble=myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+    
+}
