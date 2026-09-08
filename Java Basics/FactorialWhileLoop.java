4c50370859179377ba87d8673e8cd37c127edaa8
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-09-08 20:23:16 +0530

    9. Add program to find factorial of a number using while loop

---
+++ Java Basics/FactorialWhileLoop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorialWhileLoop{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+
+        int fact = 1;
+        int i = 1;
+
+        while(i<=num){
+            fact=fact*i;
+            i++;
+        }
+        System.out.println("Factorial : "+fact);
+    }
+}
