d411867ab9b95cd2c61bea38445fc7b1d690ac1a
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-09-21 22:05:17 +0530

    16. Add program to find GCD / HCF and LCM

---
+++ Java Basics/GCDandLCM.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class GCDandLCM{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two numbers : ");
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter b : ");
+        int b = sc.nextInt();
+
+        int gcd=1;
+
+        for(int i=1;i<=a&&i<=b;i++){
+            if(a%i==0&&b%i==0){
+                gcd=i;
+            }
+        }
+
+        int lcm = (a*b)/gcd;
+
+        System.out.println("GCD / HCF = "+gcd);
+        System.out.println("LCM = "+lcm);
+
+    }
+}
